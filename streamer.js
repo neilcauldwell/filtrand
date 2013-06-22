f657bc21806b5f2a345fe881f1b10906e08fce9b
@NeilCauldwell <neilcauldwell@gmail.com>
2013-06-22 17:27:21 +0100

    Call toSource on the tweet object.

--- streamer.js
+++ streamer.js
@@ -144,7 +144,7 @@ var tweetEmitter = function(tweet) {
     };
   }
   else {
-    sys.puts("tweetEmitter received a tweet with undefined text: " + tweet);
+    sys.puts("tweetEmitter received a tweet with undefined text: " + tweet.toSource);
   };
 };
 
