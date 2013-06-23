ebfd3cbfa3f5b0ded95839d910e37b6194478d5f
@NeilCauldwell <neilcauldwell@gmail.com>
2013-06-23 21:31:35 +0100

    Remove twitter-node integration and update subjects tracking to a separate array (there doesn't seem to be a method for returning ntwitter's tracked subjects).

--- streamer.js
+++ streamer.js
@@ -1,5 +1,4 @@
 var sys = require('sys');
-var TwitterNode = require('./vendor/twitter-node').TwitterNode;
 var ntwitter = require('ntwitter');
 var Pusher = require('node-pusher');
 
@@ -16,19 +15,18 @@ var subjectToChannel = {};
 
 var streamer = exports;
 var pusher = null;
+var subjects = [];
 var subjectsPendingDisconnection = [];
 var lastConnectionTimestamp = Date.now();
 
 // start tracking passed subject
 streamer.track = function(channel) {
   var subject = streamer.channelToSubject(channel).toLowerCase();
-  var subjects = streamer.currentSubjects();
 
   if (!includes(subject, subjects)) {
     emitEvent("subjects", "subject-subscribed", { subject: subject });
     subjects.push(subject);
-    streamer.twit = setup(subjects);
-    ntwitterSetup(subjects);
+    ntwitterConnect();
 
     if (includes(subject, subjectsPendingDisconnection)) {
       subjectsPendingDisconnection.splice(subjectsPendingDisconnection.indexOf(subject), 1);
@@ -39,7 +37,6 @@ streamer.track = function(channel) {
 // stop tracking passed subject
 streamer.untrack = function(channel) {
   var subject = streamer.channelToSubject(channel).toLowerCase();
-  var subjects = streamer.currentSubjects();
 
   if (includes(subject, subjects)) {
     emitEvent("subjects", "subject-unsubscribed", { subject: subject });
@@ -59,11 +56,6 @@ streamer.appSetup = function(key, secret, appId) {
   });
 };
 
-streamer.twitterSetup = function(username, password) {
-  streamer.twitterUsername = username;
-  streamer.twitterPassword = password;
-};
-
 streamer.ntwitterSetup = function(twitter_consumer_key, twitter_consumer_secret, twitter_access_token_key, twitter_access_token_secret) {
   streamer.twitter_consumer_key = twitter_consumer_key;
   streamer.twitter_consumer_secret = twitter_consumer_secret;
@@ -78,7 +70,6 @@ streamer.initiateReconnectionTimer = function() {
 };
 
 streamer.reconnectableSubjects = function() {
-  var subjects = streamer.currentSubjects();
   for (var i = 0; i < subjectsPendingDisconnection.length; i++) {
     subjects.splice(subjects.indexOf(subjectsPendingDisconnection[i]), 1);
   };
@@ -88,8 +79,7 @@ streamer.reconnectableSubjects = function() {
 streamer.reconnect = function() {
   var subjects = streamer.reconnectableSubjects();
   if ((subjectsPendingDisconnection.length > 0)) {
-    streamer.twit = setup(subjects);
-    ntwitterSetup(subjects);
+    ntwitterConnect();
     subjectsPendingDisconnection = [];
   };
 };
@@ -102,11 +92,6 @@ streamer.reachedReconnectionInterval = function() {
 };
 
 streamer.currentSubjects = function() {
-  var subjects = [];
-  if (streamer.twit !== undefined) {
-    subjects = streamer.twit.trackKeywords;
-  };
-
   return subjects;
 };
 
@@ -143,8 +128,6 @@ var includes = function(item, array) {
 };
 
 var tweetEmitter = function(tweet) {
-  var subjects = streamer.currentSubjects();
-
   if (tweet.text !== undefined) {
     var text = tweet.text.toLowerCase();
     for (var i in subjects) {
@@ -194,26 +177,7 @@ var emitEvent = function(channel, event, data) {
   });
 };
 
-var setup = function(subjects) {
-  var twit = new TwitterNode({
-    user: streamer.twitterUsername,
-    password: streamer.twitterPassword,
-    track: subjects
-  });
-
-  twit.addListener('error', function(error) { console.log(error.message) });
-  twit.addListener('tweet', tweetEmitter);
-  twit.addListener('end', function(resp) { sys.puts("TwitterNode client received 'end' event: " + resp.statusCode) })
-  twit.addListener('close', function(resp) { sys.puts("TwitterNode client received 'close' event.") });
-
-  twit.stream();
-  console.log("Reconnected to Twitter Stream.");
-  lastConnectionTimestamp = Date.now();
-
-  return twit;
-};
-
-var ntwitterSetup = function(subjects) {
+var ntwitterConnect = function() {
   var ntwit = new ntwitter({
     consumer_key: streamer.twitter_consumer_key,
     consumer_secret: streamer.twitter_consumer_secret,
@@ -231,5 +195,6 @@ var ntwitterSetup = function(subjects) {
     stream.on('destroy', function (response) { console.log(response) });
   });
 
-  console.log("ntwitterSetup complete.");
+  lastConnectionTimestamp = Date.now();
+  console.log("ntwitterConnect completed.");
 };
