e62360f1d6d15c626dd966bed97a04c8b30948b1
Tristian Barrett <trbarrett@gmail.com>
2013-11-15 12:03:00 +0000

    Improved the rejected sources script.

--- db-scripts/top-rejected-sources.sql
+++ db-scripts/top-rejected-sources.sql
@@ -1,6 +1,15 @@
+/* most popular filtered out sources on normal channels */
 select count(*), source 
 from filtered_tweets 
-where was_emitted = false and rejected_reason = 'source not on whitelist' 
+where was_emitted = false and rejected_reason = 'source not on whitelist' and channel not in ('#google','#free','#ipad', '#pizza', '#lol', '#remembranceday')
+group by source 
+order by count(*) desc 
+limit 20;
+
+/* most popular used sources on normal channels */
+select count(*), source 
+from filtered_tweets 
+where channel not in ('#google','#free','#ipad', '#pizza', '#lol', '#remembranceday')
 group by source 
 order by count(*) desc 
 limit 20;
